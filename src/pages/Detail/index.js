d312c874998564e58a4f1be6c1ca5f16eee681d8
Honassis Lopes <honassislopes@gmail.com>
2020-03-28 02:14:50 -0300

    conexão com BackEnd/ Scroll Inteligente

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import {Feather} from '@expo/vector-icons';
 import {Text,View,Image, TouchableOpacity, Linking} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import {useNavigation, useRoute} from '@react-navigation/native';
 import styles from './styles';
 import logoImg from '../../assets/logo.png';
 import * as MailComposer from 'expo-mail-composer';
 export default function Detail(){
     const navigation = useNavigation();
-    const message = 'Olá APAD, estou entrando em contato pois gostaria de ajudar no caso "cadelinha ferida" no valor de R$ 120,00'
+    const route = useRoute();
+    const incident = route.params.incident;
+    const MoneyValue = Intl.NumberFormat('pt-BR',
+    {style: 'currency', currency: 'BRL'})
+    .format(incident.value);
+    const message = `Olá ${incident.name}, estou entrando em contato pois gostaria de ajudar no caso "${incident.title}" no valor de ${MoneyValue}`;
     function navigateBack(){
         navigation.goBack();        
     }
     function sendMail(){
         MailComposer.composeAsync({
-            subject: ' Herói do caso: Cadelinha ferida',
-            recipients: ['honassislopes@gmail.com'],
+            subject: `Herói do caso: ${incident.title}`,
+            recipients: [incident.email],
             body: message
         })
     }
     function sendWhatsapp(){
-        Linking.openURL(`whatsapp://send?phone=558585377813&text=${message}`);
+        Linking.openURL(`whatsapp://send?phone=55${incident.whatsapp}&text=${message}`);
     }
     return (
         <View style={styles.container}>
@@ -30,14 +35,14 @@ export default function Detail(){
                 </TouchableOpacity>
             </View>
             <View style={styles.incident}>
-            <Text style={[styles.incidentProperty, {marginTop:0}]}>ONG:</Text>
-                    <Text style={styles.incidentValue}>APAD</Text>
+            <Text style={styles.incidentProperty,{marginTop:0}}>ONG:</Text>
+                    <Text style={styles.incidentValue}>{incident.name} de {incident.city}-{incident.uf}</Text>
 
                     <Text style={styles.incidentProperty}>CASO:</Text>
-                    <Text style={styles.incidentValue}>Cadelinha ferida</Text>
+                    <Text style={styles.incidentValue}>{incident.title}</Text>
 
                     <Text style={styles.incidentProperty}>VALOR:</Text>
-                    <Text style={styles.incidentValue}>R$ 120,00</Text>
+                    <Text style={styles.incidentValue}>{MoneyValue}</Text>
             </View>
             <View style={styles.contactBox}>
             <Text style={styles.heroTitle}>Salve o dia!</Text>
